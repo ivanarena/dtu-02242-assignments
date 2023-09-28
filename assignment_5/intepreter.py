1138ba16fc9e6aa31229e3996d31ec9282c2c111
ivanarena <ivan.x28x@gmail.com>
2023-09-28 14:37:25 +0200

    Add exception for first method

--- assignment_5/intepreter.py
+++ assignment_5/intepreter.py
@@ -44,7 +44,26 @@ class Interpreter:
         for method_name in self.methods_names:
             self.bytecodes[method_name] = self.methods_dict[method_name]['code']['bytecode']
 
+        self.stack = []
+
         # print(self.methods_dict[self.methods_names[0]]['code']['bytecode'])
+
+    def _push(self, op):
+        self.stack.append(op['value']['value'])
+
+    def run(self):
+        self._runBytecode(self.bytecodes['alwaysThrows1'])
+
+    def _runBytecode(self, bc):
+        for op in bc:
+            if op['opr'] == 'push':
+                self._push(op)
+            if op['opr'] == 'binary':
+                if op['operant'] == 'div':
+                    if (self.stack[-1] == 0):
+                        raise Exception("ArithmeticException")
+
+        print(self.stack)
 
     # def run(self, f: tuple[Locals, OperStack, ProgramCounter]):
     #     self.stack.append(f)
@@ -80,8 +99,9 @@ class Interpreter:
 
 
 def main():
-    program_filename = 'Array.json'
+    program_filename = 'json/Arithmetics.json'
     interpreter = Interpreter(program_filename)
+    interpreter.run()
 
 
 if __name__ == "__main__":

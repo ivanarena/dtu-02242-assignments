253bda2a32eab1c30a599870dcde3fc840144589
ivanarena <ivan.x28x@gmail.com>
2023-09-29 18:05:22 +0200

    Add neverThrow1

--- assignment_5/interpreter.py
+++ assignment_5/interpreter.py
@@ -47,10 +47,42 @@ class Interpreter:
         # print(self.methods_dict[self.methods_names[0]]['code']['bytecode'])
 
     def _push(self, op):
-        self.stack.append(op['value']['value'])
+        value = op['value']['value']
+        if value > 0:
+            self.stack.append('+')
+        elif value < 0:
+            self.stack.append('-')
+        else:
+            self.stack.append('0')
+
+    def _store(self, op):
+        if len(self.memory)-1 < op['index']:
+            self.memory.append(self.stack[-1])
+        else:
+            self.memory[op['index']] = self.stack[-1]
+
+    def _load(self, op):
+        self.stack.append(self.memory[op['index']])
+
+    def _sub(self):
+        # which one comes first?
+        if self.stack[-1] == '+' and self.stack[-2] == '+':
+            self.stack.append('0')  # suppose it's 0
+        elif self.stack[-1] == '+' and self.stack[-2] == '-':
+            print('problema')
+        elif self.stack[-1] == '-' and self.stack[-2] == '+':
+            print('no problema')
+        elif self.stack[-1] == '-' and self.stack[-2] == '-':
+            print('problema')
+
+    def _div(self):
+        if (self.stack[-1] == '0'):
+            raise Exception("ArithmeticException")
 
     def run(self):
-        for i in range(0, 2):
+        for i in range(0, 10):
+            if i in [3, 4, 5, 6, 7, 8]:
+                continue
             try:
 
                 method = self.methods_dict[self.methods_names[i]]
@@ -63,12 +95,13 @@ class Interpreter:
                 if method['params']:
                     for p in method['params']:
                         # if param is int add abstraction to stack
-                        if (p['type']['base'] == 'int'):
+                        if (p['type']['base'] == 'int' or p['type']['base'] == 'float'):
                             self.memory.append('0')
                 self._runBytecode(
                     self.bytecodes[method['name']])
+                print(f'{self.methods_names[i]}: No.')
             except Exception as e:
-                print(f'{self.methods_names[i]}: Yes, {e}')
+                print(f'{self.methods_names[i]}: Yes, {e}.')
 
     def _runBytecode(self, bc):
         for op in bc:
@@ -76,25 +109,19 @@ class Interpreter:
             if op['opr'] == 'push':
                 self._push(op)
             elif op['opr'] == 'store':
-                if len(self.memory)-1 < op['index']:
-                    self.memory.append(self.stack[-1])
-                else:
-                    self.memory[op['index']] = self.stack[-1]
+                self._store(op)
             elif op['opr'] == 'load':
-                self.stack.append(self.memory[op['index']])
+                self._load(op)
             elif op['opr'] == 'binary':
 
                 if op['operant'] == 'sub':
-                    # which one comes first?
-                    sub = self.stack[-1] - self.stack[-2]
-                    self.stack.append(sub)
+                    self._sub()
 
                 if op['operant'] == 'div':
-                    if (self.stack[-1] == 0):
-                        raise Exception("ArithmeticException")
+                    self._div()
 
-        print(self.stack)
-        print(self.memory)
+        print('stack: ', self.stack)
+        print('memory: ', self.memory)
 
     # def run(self, f: tuple[Locals, OperStack, ProgramCounter]):
     #     self.stack.append(f)
